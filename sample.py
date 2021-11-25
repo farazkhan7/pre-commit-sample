383a36e2a2dc625008daa57aba48a260461bf20f
Faraz Khan <faraz.khan@ekbana.info>
2021-11-25 10:53:48 +0545

    Initial commit

---
+++ sample.py
@@ -0,0 +1,30 @@
+"""
+This module contains the sample functions
+"""
+from datetime import date
+
+print("Beginning of a new file")
+
+
+def greet_me_with_date(name):
+    """Greets user with current date
+
+    The name of user is provided as parameter and date is taken from python
+    standard date module
+
+    Parameters
+    ----------
+    name : str
+        Name provided by the user
+
+    Returns
+    -------
+    str
+        A greeting message that combines the user's name with current date
+
+    """
+    finalstr = f"Hi {name}! Today's date is {date.today()}"
+    return finalstr
+
+
+print(greet_me_with_date("Faraz"))
